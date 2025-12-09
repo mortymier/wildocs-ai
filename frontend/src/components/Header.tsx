ba723a4a849a7114ff8b065730e4536ebeee2e48
Mortymier <sanielmg@gmail.com>
2025-12-10 03:14:56 +0800

    Frontend: add Header component

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+import citu_logo from '../assets/citu_logo.png';
+import '../styles/Header.css';
+
+export default function Header()
+{
+    return (
+        <header className="header-container">
+            <Link to="/"> <img src={citu_logo} alt="Wildocs AI logo"/> </Link>
+            <Link to="/" className="header-text"> 
+                <h1> WILDOCS AI &ensp; </h1> 
+                <h2> AI-Powered SDD Evaluator </h2> 
+            </Link>
+            <div className="header-space"></div>
+            <div className="header-buttons">
+                <Link to="/"> Login </Link>
+                <Link to="/"> Register </Link>
+            </div>
+        </header>
+    );
+}

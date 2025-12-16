e90c6c7fe459b7bbafde11a7727f2f7762e15d37
Mortymier <sanielmg@gmail.com>
2025-12-16 15:14:38 +0800

    Backend: create DTO for register request and response

---
+++ backend/src/main/java/com/wildocsai/backend/dto/RegisterResponse.java
@@ -0,0 +1,11 @@
+package com.wildocsai.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RegisterResponse
+{
+    private String message;
+}

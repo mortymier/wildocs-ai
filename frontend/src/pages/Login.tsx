0fcc776f6b9010e6a1123dfb15865f89f0bcb2d1
Mortymier <sanielmg@gmail.com>
2025-12-17 04:21:54 +0800

    Frontend: add Login page

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,53 @@
+import { Link } from 'react-router-dom';
+import Header from '../components/Header.tsx';
+import Footer from '../components/Footer.tsx';
+import email_icon from '../assets/email_icon.png';
+import password_icon from '../assets/password_icon.png';
+import '../styles/Login.css';
+
+export default function Login()
+{
+    return (
+        <>
+            <Header/>
+            <main className="login-container">
+                <h1> Welcome Wildcat </h1>
+                <p> Sign in to your WILDOCS AI account to continue </p>
+                <form>
+                    <div className="icon-label"> 
+                        <img src={email_icon} alt="Email icon"/> 
+                        <label htmlFor="email"> Email </label> 
+                    </div>
+                    <input 
+                        id="email"
+                        name="email"
+                        type="email"
+                        placeholder="your.name@school.edu"
+                        required
+                    />
+                    <div className="icon-label"> 
+                        <img src={password_icon} alt="Email icon"/> 
+                        <label htmlFor="password"> Password </label> 
+                    </div>
+                    <input 
+                        id="password"
+                        name="password"
+                        type="password"
+                        placeholder="Enter your password"
+                        required
+                    />
+                    <div className="account-actions">
+                        <input type="checkbox"/> <span> Remember me </span>
+                        <Link to="/"> Forgot password? </Link>
+                    </div>
+                    <button type="submit"> Login </button>
+                    <div className="account-actions2"> 
+                        <span> Don't have an account? </span>
+                        <Link to="/"> Sign up here </Link>
+                    </div>
+                </form>
+            </main>
+            <Footer/>
+        </>
+    );
+}

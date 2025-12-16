c00ffe22b0414480b8cad5660d5b1720837999d3
Mortymier <sanielmg@gmail.com>
2025-12-17 02:31:37 +0800

    Frontend: add Footer component

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import '../styles/Footer.css';
+
+export default function Footer()
+{
+    return (
+        <footer className="footer-container">
+            <h2> WILDOCS AI </h2>
+            <h3> WHERE ACCURACY MEETS INNOVATION </h3>
+            <hr/>
+            <p> &copy; 2025 Wildocs AI. All rights reserved. </p>
+        </footer>
+    );
+}

dc00e497924285af496866627813e14c896cbca6
Mortymier <sanielmg@gmail.com>
2025-12-17 04:22:35 +0800

    Frontend: add route for Login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.tsx';
+import Login from './pages/Login.tsx';
 
 export default function App()
 {
     return (
         <BrowserRouter>
             <Routes>
+                {/* Public Pages */}
                 <Route index element={<Home/>}/>
+                <Route path="/login" element={<Login/>}/>
             </Routes>
         </BrowserRouter>
     );

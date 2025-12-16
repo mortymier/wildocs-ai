0c26293c4a5b9260aa36a5abae383a387f04029f
Mortymier <sanielmg@gmail.com>
2025-12-17 05:19:32 +0800

    Frontend: add route for Register page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.tsx';
 import Login from './pages/Login.tsx';
+import Register from './pages/Register.tsx';
 
 export default function App()
 {
@@ -10,6 +11,7 @@ export default function App()
                 {/* Public Pages */}
                 <Route index element={<Home/>}/>
                 <Route path="/login" element={<Login/>}/>
+                <Route path="/register" element={<Register/>}/>
             </Routes>
         </BrowserRouter>
     );

fcfdd8a27c9d3ff29e99454718a40f3b377e698b
Mortymier <sanielmg@gmail.com>
2025-12-17 12:12:23 +0800

    Frontend: add route for Verify page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.tsx';
 import Login from './pages/Login.tsx';
 import Register from './pages/Register.tsx';
+import Verify from './pages/Verify.tsx';
 
 export default function App()
 {
@@ -12,6 +13,7 @@ export default function App()
                 <Route index element={<Home/>}/>
                 <Route path="/login" element={<Login/>}/>
                 <Route path="/register" element={<Register/>}/>
+                <Route path="/verify" element={<Verify/>}/>
             </Routes>
         </BrowserRouter>
     );
